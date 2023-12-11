cdc655ed1e381880bb421cd6f9457e886784d269
Dinda Yustika <84846008+Gvskaa@users.noreply.github.com>
2023-12-11 19:50:17 +0700

    Add files via upload

---
+++ file1.java
@@ -0,0 +1,10 @@
+package Homework_git;
+
+public class file1 {
+    public static void main(String[] args) {
+        String Nama = "Nama saya Dinda Yustika";
+
+        System.out.println(Nama);
+
+    }
+}
