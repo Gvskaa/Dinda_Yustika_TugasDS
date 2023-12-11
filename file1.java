468cc5dae813ab25bed997f4d8db8ec9c174be3b
Gvskaa <dindayustika04@gmail.com>
2023-12-11 20:53:13 +0700

    Update file1.java

--- file1.java
+++ file1.java
@@ -1,10 +1,12 @@
-package Homework_git;
+package Homework_git.Dinda_Yustika_TugasDS;
 
 public class file1 {
     public static void main(String[] args) {
         String Nama = "Nama saya Dinda Yustika";
-
         System.out.println(Nama);
 
+        for (int i = 0; i < 5; i++){
+            System.out.println("Hello World");
+        }
     }
 }
