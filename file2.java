9af59d6b931da09a2fd3d66d1c15ca6d07277893
Gvskaa <dindayustika04@gmail.com>
2023-12-11 20:47:42 +0700

    Create file2.java

---
+++ file2.java
@@ -0,0 +1,9 @@
+package Homework_git.Dinda_Yustika_TugasDS;
+
+public class file2 {
+    public static void main(String[] args) {
+        String Jk = "Jenis Kelamin Perempuan";
+
+        System.out.println(Jk);
+    }
+}
